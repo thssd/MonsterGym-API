40e26b7846d01b630d3d599375b785788bc59279
thssd <thiagoshimizusodre@gmail.com>
2026-07-01 17:59:31 -0300

    feat: implementa o metódo GET para verificar todos os pagamentos já efetuados

--- api/src/main/java/com/monstergym/api/controller/PagamentoController.java
+++ api/src/main/java/com/monstergym/api/controller/PagamentoController.java
@@ -1,11 +1,15 @@
 package com.monstergym.api.controller;
 
 import com.monstergym.api.domain.pagamentos.DadosCancelamentoPagamento;
+import com.monstergym.api.domain.pagamentos.DadosDetalhamentoPagamento;
 import com.monstergym.api.domain.pagamentos.DadosPagamento;
+import com.monstergym.api.repository.PagamentoRepository;
 import com.monstergym.api.service.PagametoService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +20,22 @@ public class PagamentoController {
     @Autowired
     private PagametoService service;
 
+    @Autowired
+    private PagamentoRepository repository;
+
     @PostMapping
     public ResponseEntity efetuarPagamento(@RequestBody @Valid DadosPagamento dados) {
         var dto = service.aprovarPagamento(dados);
 
         return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoPagamento>> listarPagamentos(Pageable pageable) {
+        var paginacao = repository.findAll(pageable)
+                .map(DadosDetalhamentoPagamento::new);
+
+        return ResponseEntity.ok(paginacao);
     }
 
     @DeleteMapping
--- api/src/main/java/com/monstergym/api/repository/PagamentoRepository.java
+++ api/src/main/java/com/monstergym/api/repository/PagamentoRepository.java
@@ -1,9 +1,13 @@
 package com.monstergym.api.repository;
 
 import com.monstergym.api.domain.pagamentos.Pagamento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+    Page<Pagamento> findAll(Pageable pageable);
 }
