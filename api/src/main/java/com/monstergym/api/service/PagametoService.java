b502ef27f1e77c26813d55c30c9e0b95909e658e
thssd <thiagoshimizusodre@gmail.com>
2026-07-01 17:40:13 -0300

    feat: implementa o service de pagamento com as regras de negocio

---
+++ api/src/main/java/com/monstergym/api/service/PagametoService.java
@@ -0,0 +1,59 @@
+package com.monstergym.api.service;
+
+import com.monstergym.api.domain.alunos.Planos;
+import com.monstergym.api.domain.pagamentos.DadosCancelamentoPagamento;
+import com.monstergym.api.domain.pagamentos.DadosDetalhamentoPagamento;
+import com.monstergym.api.domain.pagamentos.DadosPagamento;
+import com.monstergym.api.domain.pagamentos.Pagamento;
+import com.monstergym.api.infra.exceptions.ValidacaoException;
+import com.monstergym.api.repository.AlunoRepository;
+import com.monstergym.api.repository.PagamentoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Service
+public class PagametoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    public DadosDetalhamentoPagamento aprovarPagamento(DadosPagamento dados) {
+        if (!alunoRepository.existsById(dados.idAluno())){
+            throw new ValidacaoException("Id do aluno informado não existe.");
+        }
+        var dataHora = LocalDateTime.now();
+
+        var aluno = alunoRepository.getReferenceById(dados.idAluno());
+        var pagamento = new Pagamento(null, dados.valor(), dataHora, dados.plano(), aluno);
+
+        verificarPagamento(dados);
+
+        pagamentoRepository.save(pagamento);
+
+        return new DadosDetalhamentoPagamento(pagamento);
+    }
+
+    @Transactional
+    public void cancelarPagamento(DadosCancelamentoPagamento dados) {
+        if (!pagamentoRepository.existsById(dados.idPagamento())) {
+            throw new ValidacaoException("Pagamento não encontrado.");
+        }
+
+        pagamentoRepository.deleteById(dados.idPagamento());
+    }
+
+    //este metodo verifica se o plano do aluno condiz com o valor inserido no json
+    private void verificarPagamento(DadosPagamento dados){
+        if (!Objects.equals(dados.valor(), dados.plano().getValor())) {
+            throw new ValidacaoException(
+                    "O valor do plano " + dados.plano() + " deve ser R$ " + dados.plano().getValor());
+        }
+    }
+}
