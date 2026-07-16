a557e56013fda11cbee056e2adb61d3a9257ca63
thssd <thiagoshimizusodre@gmail.com>
2026-07-16 15:40:02 -0300

    feat: implementa testes unitarios com junit

--- api/src/test/java/com/monstergym/api/service/AvaliacaoServiceTest.java
+++ api/src/test/java/com/monstergym/api/service/AvaliacaoServiceTest.java
@@ -1,13 +1,6 @@
 package com.monstergym.api.service;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class AvaliacaoServiceTest {
 
-    @Test
-    void retornaResultado_Caso1() {
-
-    }
 }
