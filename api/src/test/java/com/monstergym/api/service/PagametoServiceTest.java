16dc5cd9d628fcbd62f47c52370701cb74db998e
thssd <thiagoshimizusodre@gmail.com>
2026-07-17 11:01:57 -0300

    feat: testes para o método aprovarPagamento

--- api/src/test/java/com/monstergym/api/service/PagametoServiceTest.java
+++ api/src/test/java/com/monstergym/api/service/PagametoServiceTest.java
@@ -1,14 +1,15 @@
 package com.monstergym.api.service;
 
+import com.monstergym.api.domain.alunos.Aluno;
+import com.monstergym.api.domain.alunos.DadosAlunos;
 import com.monstergym.api.domain.alunos.Planos;
-import com.monstergym.api.domain.pagamentos.DadosDetalhamentoPagamento;
 import com.monstergym.api.domain.pagamentos.DadosPagamento;
 import com.monstergym.api.domain.pagamentos.Pagamento;
 import com.monstergym.api.domain.pagamentos.validacoes.IValidadorPagamento;
+import com.monstergym.api.domain.treinadores.Especialidade;
 import com.monstergym.api.infra.exceptions.ValidacaoException;
 import com.monstergym.api.repository.AlunoRepository;
 import com.monstergym.api.repository.PagamentoRepository;
-import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -42,14 +43,14 @@ class PagametoServiceTest {
     private IValidadorPagamento validador2;
 
     @InjectMocks
-    private PagametoService pagametoService;
+    private PagamentoService pagamentoService;
 
     @BeforeEach
     void setUp() {
-        pagametoService = new PagametoService();
-        ReflectionTestUtils.setField(pagametoService, "alunoRepository", alunoRepository);
-        ReflectionTestUtils.setField(pagametoService, "pagamentoRepository", pagamentoRepository);
-        ReflectionTestUtils.setField(pagametoService, "validadores", List.of(validador1, validador2));
+        pagamentoService = new PagamentoService();
+        ReflectionTestUtils.setField(pagamentoService, "alunoRepository", alunoRepository);
+        ReflectionTestUtils.setField(pagamentoService, "pagamentoRepository", pagamentoRepository);
+        ReflectionTestUtils.setField(pagamentoService, "validadores", List.of(validador1, validador2));
     }
 
     @Nested
@@ -70,11 +71,86 @@ class PagametoServiceTest {
 
             //act & assert
             assertThrows(ValidacaoException.class, () ->
-                    pagametoService.aprovarPagamento(dados));
+                    pagamentoService.aprovarPagamento(dados));
 
             verify(pagamentoRepository, never()).save(any());
         }
 
+        @Test
+        @DisplayName("deve aprovar pagamento quando dados validos")
+        void aprovarPagamento_caso2() {
+            //arrange
+            var dados = new DadosPagamento(
+                    1L,
+                    100.0,
+                    LocalDateTime.now(),
+                    Planos.PADRAO
+            );
+
+            Aluno aluno = new Aluno(new DadosAlunos(
+                    "Aluno",
+                    "aluno@email",
+                    "11888888888",
+                    "12345678901",
+                    Planos.PADRAO,
+                    1.80,
+                    80.0,
+                    Especialidade.FUNCIONAL)
+            );
+            when(alunoRepository.existsById(dados.idAluno())).thenReturn(true);
+
+            when(alunoRepository.getReferenceById(dados.idAluno())).thenReturn(aluno);
+
+
+            //act
+            var output = pagamentoService.aprovarPagamento(dados);
+
+            //assert
+            assertThat(output).isNotNull();
+
+            verify(pagamentoRepository, times(1)).save(any(Pagamento.class));
+            verify(validador1).validar(dados);
+            verify(validador2).validar(dados);
+
+        }
+
+        @Test
+        @DisplayName("deve lançar exception quando validador1 rejeita")
+        void aprovarPagamento_caso3() {
+            //arrange
+            var dados = new DadosPagamento(
+                    1L,
+                    100.0,
+                    LocalDateTime.now(),
+                    Planos.PADRAO
+            );
+
+            Aluno aluno = new Aluno(new DadosAlunos(
+                    "Aluno",
+                    "aluno@email",
+                    "11888888888",
+                    "12345678901",
+                    Planos.PADRAO,
+                    1.80,
+                    80.0,
+                    Especialidade.FUNCIONAL)
+            );
+            when(alunoRepository.existsById(dados.idAluno())).thenReturn(true);
+
+            when(alunoRepository.getReferenceById(dados.idAluno())).thenReturn(aluno);
+
+            //act
+            doThrow(new ValidacaoException("O valor do plano "
+                    + dados.plano() + " deve ser R$ " + dados.plano().getValor()))
+                    .when(validador1).validar(dados);
+
+            //assert
+            assertThrows(ValidacaoException.class, () ->
+                    pagamentoService.aprovarPagamento(dados));
+
+            verify(pagamentoRepository, never()).save(any());
+
+        }
     }
 
     @Nested
