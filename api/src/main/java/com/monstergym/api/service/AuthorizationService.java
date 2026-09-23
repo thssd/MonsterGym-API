8ef7855f369eed53b09e09c75b8c505ceba2ee17
thssd <thiagoshimizusodre@gmail.com>
2026-09-23 10:42:39 -0300

    feat: implementa 'orElseThrow' método para tratar exceptions

--- api/src/main/java/com/monstergym/api/service/AuthorizationService.java
+++ api/src/main/java/com/monstergym/api/service/AuthorizationService.java
@@ -15,6 +15,6 @@ public class AuthorizationService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByUsername(username);
+        return repository.findByUsername(username).orElseThrow();
     }
 }
