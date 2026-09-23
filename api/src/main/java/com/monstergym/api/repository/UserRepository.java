e7dc63637f9e8953f698c0e4abe8fb5bcdf03ab1
thssd <thiagoshimizusodre@gmail.com>
2026-09-23 10:54:25 -0300

    fix: a query se torna um Optional para o recebimento da resposta.

--- api/src/main/java/com/monstergym/api/repository/UserRepository.java
+++ api/src/main/java/com/monstergym/api/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.monstergym.api.repository;
 
 import com.monstergym.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    UserDetails findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
